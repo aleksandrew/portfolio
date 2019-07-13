e8bce16b71f2a36f0f699a197b43609abd9c547f
Andrey Aleksandrov <andrew.aleksandrow97@gmail.com>
2019-07-13 10:42:44 +0300

    Remove the now ignored directory "src"

--- src/js/pages/main.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-import {scrollAnimate} from '../module/scrollAnimate';
-
-export default $(() => {
-	const menuItems = $("#scrollToAbout");
-
-	scrollAnimate(menuItems);
-})
